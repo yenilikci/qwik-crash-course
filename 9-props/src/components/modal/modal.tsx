743764bbf4b1f34bbcf5a743123556fe0049030b
yenilikci <m.melihcelik017@outlook.com>
2023-07-01 00:27:26 +0300

    lesson 9

---
+++ 9-props/src/components/modal/modal.tsx
@@ -0,0 +1,26 @@
+import { component$, useStylesScoped$, Slot } from "@builder.io/qwik";
+import modalStyles from './modal.css?inline'
+
+interface ModalProps {
+    size: 'sm' | 'lg',
+    frosted?: boolean,
+}
+
+export default component$((props: ModalProps) => {
+    useStylesScoped$(modalStyles)
+
+    return (
+        <div class={`modal ${props.size} ${props.frosted && 'frosted'}`}>
+            <div class="modal-content">
+                <div class="close">close</div>
+                {/* <Slot /> */}
+                <main class="main-content">
+                    <Slot name="content" />
+                </main>
+                <footer>
+                    <Slot name="footer" />
+                </footer>
+            </div>
+        </div>
+    )
+})
