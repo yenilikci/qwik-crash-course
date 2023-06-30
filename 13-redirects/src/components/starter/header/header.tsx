e4364cf93d9fbda2c75142fab5cf3271cf729e4b
yenilikci <m.melihcelik017@outlook.com>
2023-07-01 01:49:50 +0300

    lesson 13

---
+++ 13-redirects/src/components/starter/header/header.tsx
@@ -0,0 +1,28 @@
+import { component$, useStyles$, useStylesScoped$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import styles from "./header.css?inline";
+
+export default component$(() => {
+
+  useStyles$(styles);
+
+  return (
+    <header>
+      <nav>
+        <img src="/mario_block.jpeg" alt="logo" />
+        <h1>Mario Life</h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+});
