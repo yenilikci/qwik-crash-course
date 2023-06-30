14226280f54c4849783d5065c30c7f7553caaa82
yenilikci <m.melihcelik017@outlook.com>
2023-07-01 00:39:23 +0300

    lesson 10

---
+++ 10-functions-qrls/src/routes/index.tsx
@@ -0,0 +1,48 @@
+import { component$, useSignal, useStore } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+
+  const name = useSignal('mario')
+
+  const person = useStore({
+    name: 'peach',
+    age: 30
+  })
+
+  const blogs = useStore([
+    { id: 1, title: 'my first blog' },
+    { id: 2, title: 'my second blog' },
+    { id: 3, title: 'marmite rules' },
+  ])
+
+  return (
+    <div>
+      <h1>Okie Dokie!</h1>
+
+      <p>Hello, {name.value}</p>
+      <p>Hello, {person.name} - {person.age}</p>
+
+      <button onClick$={() => name.value = 'luigi'}>click me</button>
+      <button onClick$={() => person.name = 'bowser'}>click me again</button>
+
+      {blogs.map(blog => (
+        <div key={blog.id}>
+          {blog.title}
+        </div>
+      ))}
+
+      <button onClick$={() => blogs.pop()}>remove a blog</button>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Mario Life',
+  meta: [
+    {
+      name: 'description',
+      content: 'a blog site about everything Super Mario related',
+    },
+  ]
+};
---
+++ 10-functions-qrls/src/routes/layout-test.tsx
@@ -0,0 +1,19 @@
+import { component$, Slot } from '@builder.io/qwik';
+import Header from '~/components/starter/header/header';
+
+export default component$(() => {
+    return (
+        <>
+            <main>
+                <Header />
+                <p>THIS IS THE TEST LAYOUT</p>
+                <section class="container">
+                    <Slot />
+                </section>
+            </main>
+            <footer>
+                <p>Copyright 2023 Mario Life</p>
+            </footer>
+        </>
+    );
+});
